83db3536e08a5bd00f341d639d4c11433618f01c
Bohdan_Zaharkiv <Bohdan_Zaharkiv@epam.com>
2017-09-22 17:44:42 +0300

    Add Spring Repository dependency

---
+++ messenger-user-service/src/main/java/com/epam/mentorship/user/service/dao/UserDao.java
@@ -0,0 +1,11 @@
+package com.epam.mentorship.user.service.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.epam.mentorship.common.model.User;
+
+@Repository
+public interface UserDao extends JpaRepository<User, Long> {
+
+}
