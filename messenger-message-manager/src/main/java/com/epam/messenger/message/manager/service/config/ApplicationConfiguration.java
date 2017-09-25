36e1dde873a0eec99f84b0bbe1a8738c37b045f1
Bohdan_Zaharkiv <Bohdan_Zaharkiv@epam.com>
2017-09-25 12:06:45 +0300

    Add Spring properties. Create configuration for message manager

---
+++ messenger-message-manager/src/main/java/com/epam/messenger/message/manager/service/config/ApplicationConfiguration.java
@@ -0,0 +1,10 @@
+package com.epam.messenger.message.manager.service.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.epam.messenger")
+public class ApplicationConfiguration {
+
+}
---
+++ messenger-message-manager/src/main/java/com/epam/messenger/message/manager/service/config/MessageManagerApplication.java
@@ -0,0 +1,12 @@
+package com.epam.messenger.message.manager.service.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageManagerApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(MessageManagerApplication.class, args);
+    }
+}
