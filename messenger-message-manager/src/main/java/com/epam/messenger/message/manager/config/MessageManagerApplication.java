70ff429112f4b0921fb0feb0bd58208cd4ae3304
Bohdan_Zaharkiv <Bohdan_Zaharkiv@epam.com>
2017-10-02 18:06:12 +0300

    Add eureka discovery service

--- messenger-message-manager/src/main/java/com/epam/messenger/message/manager/config/MessageManagerApplication.java
+++ messenger-message-manager/src/main/java/com/epam/messenger/message/manager/config/MessageManagerApplication.java
@@ -2,8 +2,10 @@ package com.epam.messenger.message.manager.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MessageManagerApplication {
 
   public static void main(final String[] args) {
--- messenger-user-service/src/main/java/com/epam/messenger/user/management/config/UserManagementApplication.java
+++ messenger-user-service/src/main/java/com/epam/messenger/user/management/config/UserManagementApplication.java
@@ -2,8 +2,10 @@ package com.epam.messenger.user.management.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserManagementApplication {
 
   public static void main(String[] args) {
