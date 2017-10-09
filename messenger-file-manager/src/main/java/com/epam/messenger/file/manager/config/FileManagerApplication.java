5467ba2a538f5bd08fa5cbc7c26c71123c60f089
Bohdan_Zaharkiv <Bohdan_Zaharkiv@epam.com>
2017-10-09 18:04:32 +0300

    Enable Eureka for file manager

--- messenger-file-manager/src/main/java/com/epam/messenger/file/manager/config/FileManagerApplication.java
+++ messenger-file-manager/src/main/java/com/epam/messenger/file/manager/config/FileManagerApplication.java
@@ -3,9 +3,11 @@ package com.epam.messenger.file.manager.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
+@EnableDiscoveryClient
 public class FileManagerApplication {
 
   public static void main(String[] args) {
